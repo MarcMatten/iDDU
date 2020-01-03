- adjusted calculation timestep for raceLapsEstimation (5 -> 15 sec) - pace car is rendered last on map --> always on top - adjusted for some labels not to display negative values - corrected green pit stop indicator on map for zero required pit stops - calculated race lap number is now used in the lab LabeledValue

libs/raceLapsEstimation.py
@@ -17,8 +17,8 @@ class raceLapsEstimation(threading.Thread):
     def run(self):
         while 1:
             if self.db.BDDUexecuting:
-                if self.db.SessionInfo['Sessions'][self.db.SessionNum]['ResultsPositions']:
-                # if self.db.SessionInfo['Sessions'][self.db.SessionNum]['ResultsPositions'] and self.db.SessionInfo['Sessions'][self.db.SessionNum]['SessionType'] == 'Race' and self.db.TimeLimit:
+                # if self.db.SessionInfo['Sessions'][self.db.SessionNum]['ResultsPositions']:
+                if self.db.SessionInfo['Sessions'][self.db.SessionNum]['ResultsPositions'] and self.db.SessionInfo['Sessions'][self.db.SessionNum]['SessionType'] == 'Race' and self.db.TimeLimit:
                     for i in range(0, len(self.db.SessionInfo['Sessions'][self.db.SessionNum]['ResultsPositions'])):
                         CarIdx_temp = self.db.SessionInfo['Sessions'][self.db.SessionNum]['ResultsPositions'][i]['CarIdx']
                         temp_CarIdxtLap = self.db.SessionInfo['Sessions'][self.db.SessionNum]['ResultsPositions'][i]['LastTime']
@@ -28,33 +28,33 @@ class raceLapsEstimation(threading.Thread):
                         self.db.CarIdxtLap[CarIdx_temp][self.db.SessionInfo['Sessions'][self.db.SessionNum]['ResultsPositions'][i]['LapsComplete'] - 1] = temp_CarIdxtLap
 
                         # if self.db.TimeLimit:  # and not self.db.LapLimit:
-                        try:
-                            temp_pitstopsremain_np = self.db.PitStopsRequired - np.array(self.db.CarIdxPitStops[CarIdx_temp])
-                            temp_pitstopsremain = temp_pitstopsremain_np.tolist()
-                            NLapTimed = np.count_nonzero(~np.isnan(self.db.CarIdxtLap[CarIdx_temp]))
+                        # try:
+                        temp_pitstopsremain_np = self.db.PitStopsRequired - np.array(self.db.CarIdxPitStops[CarIdx_temp])
+                        temp_pitstopsremain = temp_pitstopsremain_np.tolist()
+                        NLapTimed = np.count_nonzero(~np.isnan(self.db.CarIdxtLap[CarIdx_temp]))
 
-                            # summed up laptime - remove nan first
-                            CarIdxtLap_cleaned = [x for x in self.db.CarIdxtLap[CarIdx_temp] if str(x) != 'nan']
-                            CarIdxtLapSum = np.sum(CarIdxtLap_cleaned)
+                        # summed up laptime - remove nan first
+                        CarIdxtLap_cleaned = [x for x in self.db.CarIdxtLap[CarIdx_temp] if str(x) != 'nan']
+                        CarIdxtLapSum = np.sum(CarIdxtLap_cleaned)
 
-                            # use this to find winner
-                            self.db.NLapRaceTime[CarIdx_temp] = (self.db.SessionLength - CarIdxtLapSum - (
-                                    iDDUhelper.maxList(temp_pitstopsremain,
-                                                       0) * self.db.PitStopDelta)) / iDDUhelper.meanTol(
-                                self.db.CarIdxtLap[CarIdx_temp], 0.03) + NLapTimed - self.db.ResultsPositions[i]['Lap']  # use this to find winner
+                        # use this to find winner
+                        self.db.NLapRaceTime[CarIdx_temp] = (self.db.SessionLength - CarIdxtLapSum - (
+                                iDDUhelper.maxList(temp_pitstopsremain,
+                                                   0) * self.db.PitStopDelta)) / iDDUhelper.meanTol(
+                            self.db.CarIdxtLap[CarIdx_temp], 0.03) + NLapTimed - self.db.SessionInfo['Sessions'][self.db.SessionNum]['ResultsPositions'][i]['Lap']  # use this to find winner
 
-                            # if my value lower than the winners, then + 1 lap
-                            self.db.TFinishPredicted[CarIdx_temp] = (np.ceil(
-                                self.db.NLapRaceTime[CarIdx_temp]) - NLapTimed) * iDDUhelper.meanTol(self.db.CarIdxtLap[CarIdx_temp], 0.03) + CarIdxtLapSum + (
-                                                                            iDDUhelper.maxList(temp_pitstopsremain, 0) * self.db.PitStopDelta)
-                        except:
-                            print('exception in iDDUcalc!')
-                            print('i:')
-                            print(i)
-                            if not self.snapshot:
-                                self.db.snapshot()
-                                print('RTDB snapshot saved!')
-                                self.snapshot = True
+                        # if my value lower than the winners, then + 1 lap
+                        self.db.TFinishPredicted[CarIdx_temp] = (np.ceil(
+                            self.db.NLapRaceTime[CarIdx_temp]) - NLapTimed) * iDDUhelper.meanTol(self.db.CarIdxtLap[CarIdx_temp], 0.03) + CarIdxtLapSum + (
+                                                                        iDDUhelper.maxList(temp_pitstopsremain, 0) * self.db.PitStopDelta)
+                        # except:
+                        #     print('exception in raceLapsEstimation!')
+                        #     print('i:')
+                        #     print(i)
+                        #     if not self.snapshot:
+                        #         self.db.snapshot()
+                        #         print('RTDB snapshot saved!')
+                        #         self.snapshot = True
 
                     self.db.WinnerCarIdx = self.db.NLapRaceTime.index(max(self.db.NLapRaceTime))
 
@@ -73,6 +73,10 @@ class raceLapsEstimation(threading.Thread):
                     else:
                         self.db.NLapDriver = float(self.db.NLapWinnerRaceTime)
 
-                    print(self.db.timeStr + ':\tinit WinnerCarIdx: ' + str(self.db.WinnerCarIdx))
-                    print(self.db.timeStr + ':\tinit NLapDriver: ' + str(self.db.NLapDriver))
+                    if self.db.NLapDriver > 1:
+                        self.db.RaceLaps = int(self.db.NLapDriver + 1)
+                    else:
+                        self.db.RaceLaps = self.db.UserRaceLaps
+
+
             time.sleep(self.rate)